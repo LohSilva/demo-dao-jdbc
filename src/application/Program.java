e410275c9800d201f6dc44294d173fa3d27644b4
Loh Silva <lo-ruama_sp@hotmail.com>
2023-08-05 21:11:14 -0300

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,8 +29,7 @@ public class Program {
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
-		}
-		
+		}		
 		
 		System.out.println("\n=== TEST 4: Seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
